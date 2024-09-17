32c6be842b99b38e22d5d230fd7d391ff89d3146
Johannes Rosing <jrosing@kth.se>
2024-09-17 21:17:54 +0200

    Added secret.py

--- vibra_backend/settings.py
+++ vibra_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from secret import my_ip
+from super_secret import my_ip
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
