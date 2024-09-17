691291ddf787402b93c9659f138bb3627e9fca63
Johannes Rosing <jrosing@kth.se>
2024-09-17 19:36:57 +0200

    Create WelcomeView as first API View

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+# NOTE: Test view
+class WelcomeView(APIView):
+    def get(self, request):
+        data = {
+            "message": "Welcome to Vibra App!"
+        }
+        return Response(data)
