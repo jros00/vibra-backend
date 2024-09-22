497dd9aa7c29f804a422af7fa61798ab1df7a440
Emilia Kallis <emilia.kallis@hotmail.com>
2024-09-22 12:31:10 +0200

    changed names of config classes

--- ml_tools_app/apps.py
+++ ml_tools_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HomeConfig(AppConfig):
+class MlToolsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ml_tools_app'
