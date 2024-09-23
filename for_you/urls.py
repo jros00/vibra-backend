325979d71138088612111da338ed75a4390cf788
Johannes Rosing <jrosing@kth.se>
2024-09-23 02:24:11 +0200

    registered new views

--- for_you/urls.py
+++ for_you/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path, include
-from .views.advertisement_views import AdvertisementView
-from .views.content_views import PredictionViewSet
-from .views.mood_views import ChangeMoodView
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .views.advertisement_views import AdvertisementView
+from .views.content_views import FetchRecommendedTracksView, FetchTrackView, FirstTrackView
+from .views.mood_views import ChangeMoodView
+
 router = DefaultRouter()
-router.register(r'for_you', PredictionViewSet, basename='for_you')
+router.register(r'first_track', FirstTrackView, basename='first_track')
+router.register(r'recommended', FetchRecommendedTracksView, basename='recommended')
+router.register(r'track', FetchTrackView, basename='track')
 
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('advertisements/', AdvertisementView.as_view(), name='home'),
-    path('change-mood/', ChangeMoodView.as_view(), name='mood'),
     path('', include(router.urls)),
 ]
