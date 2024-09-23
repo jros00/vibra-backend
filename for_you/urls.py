d4113c72c121341061b43344f6d7916a4fcc8189
Johannes Rosing <jrosing@kth.se>
2024-09-23 21:05:35 +0200

    Removed FirstTrackView

--- for_you/urls.py
+++ for_you/urls.py
@@ -2,11 +2,10 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views.advertisement_views import AdvertisementView
-from .views.content_views import FetchRecommendedTracksView, FetchTrackView, FirstTrackView
+from .views.content_views import FetchRecommendedTracksView, FetchTrackView
 from .views.mood_views import ChangeMoodView
 
 router = DefaultRouter()
-router.register(r'first_track', FirstTrackView, basename='first_track')
 router.register(r'recommended', FetchRecommendedTracksView, basename='recommended')
 router.register(r'track', FetchTrackView, basename='track')
 
