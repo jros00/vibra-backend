0f14c9fd5c42df48c56cd242facbbb65eb8dffca
Emilia Kallis <emilia.kallis@hotmail.com>
2024-10-17 21:36:30 +0200

    Added request example

--- profile2/urls.py
+++ profile2/urls.py
@@ -2,7 +2,18 @@
 from django.urls import path
 from .views import profile_view, edit_biography_view
 
+'''
+Using pk instead of username is more of convension. You might need to update the import if you use viewset or apiview.
+path('<str:pk>/', profile_view, name='profile'),
+'''
 urlpatterns = [
     path('<str:username>/', profile_view, name='profile'),
     path('edit/', edit_biography_view, name='edit_biography'),
 ]
+
+# http://localhost:8000/profile/laura/ GET 
+# http://localhost:8000/profile/edit/ POST 
+
+# {
+#     "biography": "Some description of me."
+# }
